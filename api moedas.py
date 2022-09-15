005056ca11ddf9d4885991ed705f1e104422201f
ericdepaula <ericdepaula39@gmail.com>
2022-09-15 09:17:13 -0300

    Adiconando cotação do Euro

--- api moedas.py
+++ api moedas.py
@@ -5,3 +5,8 @@ cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,
 cotacao = cotacao.json()
 cotacao_dol = cotacao['USDBRL']['bid']
 print("Cotação do Dólar:", cotacao_dol)
+
+cotacao2 = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+cotacao2 = cotacao2.json()
+cotacao_eur = cotacao2['EURBRL']['bid']
+print("Cotação do Euro:", cotacao_eur)
