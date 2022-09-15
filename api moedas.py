ba8fce8bdc8fa80691095d2e39bfaf21e1e8755f
ericdepaula <ericdepaula39@gmail.com>
2022-09-14 21:25:17 -0300

    Alteração Feita

--- api moedas.py
+++ api moedas.py
@@ -4,4 +4,4 @@ import json
 cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cotacao = cotacao.json()
 cotacao_dol = cotacao['USDBRL']['bid']
-print(cotacao_dol)
+print("Price dol now:", cotacao_dol)
