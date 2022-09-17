96518722ab866be93caf7fc8bfdf020c8f50100b
ericdepaula <ericdepaula39@gmail.com>
2022-09-17 11:57:58 -0300

    Cotação do dólar a cada minuto

--- api moedas.py
+++ api moedas.py
@@ -1,12 +1,23 @@
 import requests
 import json
+import time
 
-cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-cotacao = cotacao.json()
-cotacao_dol = cotacao['USDBRL']['bid']
-print("Cotação do Dólar:", cotacao_dol)
+roda = True
 
-cotacao2 = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-cotacao2 = cotacao2.json()
-cotacao_eur = cotacao2['EURBRL']['bid']
-print("Cotação do Euro:", cotacao_eur)
+while(roda):
+    def countdown(num_of_secs):
+        while num_of_secs:
+            m, s = divmod(num_of_secs, 60)
+            min_sec_format = '{:02d}:{:02d}'.format(m, s)
+            # print(min_sec_format)
+            time.sleep(1)
+            num_of_secs -= 1
+            
+    countdown(60)
+
+    cotacao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    cotacao = cotacao.json()
+    cotacao_dol = cotacao['USDBRL']['bid']
+    print('Cotação do Dólar:', cotacao_dol)
+
+roda = countdown()
